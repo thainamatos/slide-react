3ad98d8944dcfef90a31849795077cea6d072e43
Thainá Matos <thainamatos.if@hotmail.com>
2022-11-03 11:19:00 -0300

    slide simples

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Slide from './Slide';
 
 function App() {
+  const slides = [
+    {
+        id: "slide1",
+        text:'Slide 1'
+    },
+    {
+        id: "slide2",
+        text:'Slide 2'
+    },
+    {
+        id: "slide3",
+        text:'Slide 3'
+    }
+]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Slide slides={slides}/>
     </div>
   );
 }
